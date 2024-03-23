d75aa64ca8a670a20e40fa094aeb0306ad54ce0d
nmanyange <nmanyange30@gmail.com>
2024-03-23 03:25:17 -0700

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,18 @@
 #!/usr/bin/pup
 # Using puppet to install flask and werkzeug with specific versions
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+exec {'install_flask':
+  command => '/usr/bin/pip3
+install flask == 2.1.0',
+  path => ['/usr/bin', '/bin'],
+  unless => '/usr/bin/pip3 show 
+flask | grep -q "Version: 2.1.0"',
 }
 
-package {'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
+exec {'install_werkzeug':
+  command => '/usr/bin/pip3
+install werkzeug==2.1.1',
+  path => ['/usr/bin', '/bin']
+  unless => '/usr/bin/pip3 show
+werkzeug | grep -q "Version: 2.1.1"',
 }
